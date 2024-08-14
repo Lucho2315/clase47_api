9c7c1493ddaa404dbf953f06bf15b88e07cb9190
LUIS ENRIQUE MARQUEZ <luismarquez98@hotmail.com>
2024-08-14 07:27:54 -0500

    Creacion metodo post para crear mascotas

---
+++ models/mascota.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema= mongoose.Schema;
+
+const mascotaSchema=new Schema({
+    nombre:{
+        type: String,
+        required:true
+    },
+    descripcion:{
+        type: String,
+        required: true
+    }    
+   
+});
+
+/*Creacion modelo*/
+
+       const Mascota= mongoose.model("Mascota.", mascotaSchema);
+       module.exports=Mascota
